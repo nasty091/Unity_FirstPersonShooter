6c932b13e598d963c8bab47e0e8d828d300f1002
nasty091 <6151071035@st.utc2.edu.vn>
2023-05-20 17:35:36 +0700

    Adding Gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed, gravityModifier;
     public CharacterController charCon;
 
     private Vector3 moveInput;
@@ -26,15 +26,26 @@ public class PlayerController : MonoBehaviour
         //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
+        //Store y velocity, because at line 36 moveInput.y will be 0 so yStore will back up the value of moveInput.y
+        float yStore = moveInput.y;
+       
         //Set up movement and can change direction
         Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
         Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
-
-        moveInput = vertMove + horiMove;
-        //Debug.Log("moveInput before Normalize: " + moveInput.x + " " + moveInput.y + " " + moveInput.z);
+       
+        moveInput = vertMove + horiMove;//moveInput.y = 0 Because vertMove and horiMove just change the x and z
         moveInput.Normalize(); // Set the value of moveInput = 1 or smaller 
-        //Debug.Log("moveInput after Normalize: " + moveInput.x + " " + moveInput.y + " " + moveInput.z);
         moveInput = moveInput * moveSpeed;
+
+        //Set up gravity for player
+        moveInput.y = yStore;
+
+        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+        
+        if (charCon.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
 
         charCon.Move(moveInput * Time.deltaTime);
 
