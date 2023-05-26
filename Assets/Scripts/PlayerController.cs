8bcf892a1cfedcaea2d17db8d50f7e1fde673a67
nasty091 <6151071035@st.utc2.edu.vn>
2023-05-26 22:11:48 +0700

    Changing Projectile Direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -102,6 +102,19 @@ public class PlayerController : MonoBehaviour
         //Handle Shooting
         if(Input.GetMouseButtonDown(0))
         {
+            RaycastHit hit;
+            if(Physics.Raycast(camTrans.position, camTrans.forward, out hit, 50f))
+            {
+                if(Vector3.Distance(camTrans.position, hit.point) > 2f)
+                {
+                    firePoint.LookAt(hit.point);
+                }
+            }
+            else
+            {
+                firePoint.LookAt(camTrans.position + (camTrans.forward * 30f));
+            }
+
             Instantiate(bullet, firePoint.position, firePoint.rotation);
         }
 
